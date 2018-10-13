16cd3db67beb57db8eae186e3c32d8fe8e1bfd92
Jared <jared.harris1@g.austincc.edu>
2018-10-12 21:24:49 -0500

    movies working

--- liri.js
+++ liri.js
@@ -1,48 +1,94 @@
 require("dotenv").config();
 
-let keys = require('./keys')
+/*Global Variables
+==============================================================*/
+//Packages and Modules
+var fs = require('fs');
+var spotify = require('spotify');
+//var spotify = new Spotify(keys.spotify);
+//var twitter = require('twitter');
+var keys = require('./keys');
+//let moment = require('moment');
+var request = require('request');
 const chalk = require('chalk');
 
-
-//var spotify = new Spotify(keys.spotify);
+//Inputs
+let inputs = process.argv[2];
 
 
 //-------Functions--------
+function liriMeThis() {
+    switch (inputs) {
+        case "movie-this":
+            omdbAPI();
+            break;
 
-//-------Movies---------
-let request = require("request");
-const movieName = process.argv.splice(2);
-const bandName = process.argv.splice(2);
-const queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-const bandsapi = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        case "concert-this":
+            bandsInTownAPI();
+            break;
 
+        case "spotify-this":
+            spotifyMe();
+            break;
 
-request(queryUrl, (err, response, body) =>{
-   if( err )
-       return err;
-
-   if( response.statusCode === 200 ){
-       let movieName = JSON.parse(body);
-
-       console.log(chalk.yellow(movieName.tomatoURL))
-       console.log(chalk.green('Movie Title: ' + movieName.Title))
-       console.log(chalk.yellow('Year: ' + movieName.Year))
-       console.log(chalk.yellow('Rated: ' + movieName.Rated))
-       console.log(chalk.yellow('IMDB Rating: ' + movieName.imdbRating))
-       console.log(chalk.yellow('Country: ' + movieName.Country))
-       console.log(chalk.yellow('Languages: ' + movieName.Language))
-       console.log(chalk.yellow('Plot: ' + movieName.Plot))
-       console.log(chalk.yellow('Actors: ' + movieName.Actors))
-   }
-})
-//--------Bands----
-request(bandsapi, (err, response, body) => {
-    if (err)
-        return err;
-
-    if (response.statusCode === 200) {
-        let artist = JSON.parse(bandName.body);
-
-        console.log(body)
     }
-})
+}
+//////////////////// M O V I E S ////////////////////
+function omdbAPI() {
+    let movieName = process.argv.splice(3);
+    const queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    request(queryUrl, (err, response, body) => {
+
+        if (!err && response.statusCode === 200) {
+            let movieName = JSON.parse(body);
+            let movieData = (`
+       ${(chalk.green('Movie Title: ' + movieName.Title))}
+       ${(chalk.yellow('Year: ' + movieName.Year))}
+       ${(chalk.yellow('Rated: ' + movieName.Rated))}
+       ${(chalk.yellow('IMDB Rating: ' + movieName.imdbRating))}
+       ${(chalk.yellow("Rotten Tomato Rating: " + movieName.Ratings[1].Value))}
+       ${(chalk.yellow('Country: ' + movieName.Country))}
+       ${(chalk.yellow('Languages: ' + movieName.Language))}
+       ${(chalk.yellow('Plot: ' + movieName.Plot))}
+       ${(chalk.yellow('Actors: ' + movieName.Actors))}
+       `);
+
+            console.log(movieData);
+        };
+        if (err) {
+            console.log("Are you sure that's the right movie?!")
+        };
+    });
+};
+
+
+///////////////// B A N D S ////////////////////
+function bandsInTownAPI() {
+    var artist = process.argv.splice(3);
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    request(queryUrl, (err, response, body) => {
+        if (err)
+            return err;
+
+        if (!err && response.statusCode === 200) {
+            var artist = JSON.parse(body);
+
+            console.log(artist);
+            console.log(body);
+        }
+    })
+}
+
+/////////////// S P O T I F Y //////////////////
+function spotifyMe (){
+    let track = process.argv.splice(3);
+    if (!track){
+        track = "Thriller"
+    }
+    
+}
+
+
+liriMeThis();
