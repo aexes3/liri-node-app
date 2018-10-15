a534333c9f0e4f941341288affbac3041b47817c
Jared <jared.harris1@g.austincc.edu>
2018-10-14 22:30:22 -0500

    concert-this, movie-this & spotify-this working

--- liri.js
+++ liri.js
@@ -1,16 +1,15 @@
-require("dotenv").config();
-
 /*Global Variables
 ==============================================================*/
 //Packages and Modules
-var fs = require('fs');
-var keys = require('./keys');
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
 //var twitter = require('twitter');
-let moment = require('moment');
+require("dotenv").config();
+var keys = require('./keys');
+var fs = require('fs');
 var request = require('request');
 const chalk = require('chalk');
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+let moment = require('moment');
 
 //Inputs
 let inputs = process.argv[2];
@@ -65,59 +64,61 @@ function omdbAPI() {
 
 ///////////////// B A N D S ////////////////////
 function bandsInTownAPI() {
-    let artist = process.argv.splice(3);
+    let artist = process.argv.splice(3, process.argv.length - 1).join(" ");
     var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
     request(queryUrl, (err, response, body) => {
+        if (artist === "" || err) {
+            console.log("Try again")
+            return
+        };
+
         if (!err && response.statusCode === 200) {
             let bandName = JSON.parse(body);
 
-           for(let i=0; i<3; i++){
-            //    console.log(bandName);
-            // console.count('band')
-            console.log(bandName[i].venue.name);
-            console.log("Location: " + bandName[i].venue.city);
-            console.log(moment(bandName[i].datetime).format("MM/DD/YY"));
+            for (let i = 0; i < 3; i++) {
+                console.count('Concert #')
+
+                let concert = (`
+            ${(chalk.green("Artist: " + artist))}
+            ${(chalk.yellow("Venue: " + bandName[i].venue.name))}
+            ${(chalk.yellow("Location: " + bandName[i].venue.city + ", " + bandName[i].venue.region))}
+            ${(chalk.yellow("Date: " + moment(bandName[i].datetime).format("MM/DD/YY")))}
+            ${("\n==========================================\n")}
+            `);
+
+                console.log(concert);
             }
+        }
+        if (err) {
+            console.log("Uuuuuuuuuh oh, you need to try again!")
         }
     })
 }
 
 /////////////// S P O T I F Y //////////////////
-function spotifyMe (){
-    let track = process.argv.splice(3);
-    spotify.search({type:'track', query: track}, function(err, data){
-                    
-        if(!err) {
-          displaySpotify(data);
+function spotifyMe() {
+    let tuneQuery = process.argv.splice(3, process.argv.length - 1).join(" ");
+        if(!tuneQuery){
+            tuneQuery = "Thriller"
         }
-     
+        //why does this spotify package only accept track, not artist or album?
+     spotify.search({ type: "track", query: tuneQuery}, (err, data) => {
+        if (err) {
+           console.log("SYup, I knew something like this would happen: " + err);
+           return;
+        }
         else {
-          throw err;
+            songInfo = (`
+            ${(chalk.green("Song Title: " + tuneQuery))}
+            ${(chalk.yellow("Artist: " + data.tracks.items[0].album.artists[0].name))}
+            ${(chalk.yellow("Album: " + data.tracks.items[0].album.name))}
+            ${(chalk.yellow("Spotify This Song: " + data.tracks.items[0].album.external_urls.spotify))}
+            `);
+    
+            console.log(songInfo);
         }
-  });
-  spotify.search({type:'track', query: "Thiriller"}, function(err,data){
-      if(!err){
-          displaySpotify(data);
-        }
-        else{
-            throw err;
-        }
-    });
-}
-function displaySpotify(data){
-    // console.log(data);
-    // console.log('keys!',data.tracks.items[0]);
-    var artists = data.tracks.items[0].artists[0].name;             //['tracks']['items'][1]['artists'][0]['name'];
-    var album = data.tracks.items[0].artists[0].name;               //['tracks']['items'][1]['album']['name'];
-    var songPreview = data.tracks.items[0].external_urls.spotify;   //['tracks']['items'][1]['external_urls']['spotify'];
-    var track = data.tracks.items[0].name;                          //['tracks']['items'][1]['name'];
-    //Testing
-    console.log(JSON.stringify(data['tracks']['items'][1]['name'] ,null, 1));
-    console.log('Artist: ' + artists);
-    console.log('Track: ' + track);
-    console.log('Album ' + album);
-    console.log('Song Preview: ' + songPreview);
-}
+   }) 
+};
 
 liriMeThis();
